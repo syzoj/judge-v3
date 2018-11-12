3ad216734073d12362f68359d2c8bc06396f0515
Menci <huanghaorui301@gmail.com>
2018-11-12 22:03:05 +0800

    Remove frontend

--- src/frontend-syzoj/cleanup.ts
+++
@@ -1,8 +0,0 @@
-import {disconnect as disconnectRMQ } from './rmq';
-import winston = require('winston');
-
-export function cleanUp(retCode: number) {
-    winston.info('Cleaning up...');
-    disconnectRMQ();
-    process.exit(1);
-}
