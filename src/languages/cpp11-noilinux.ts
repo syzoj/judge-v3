ddf1c4f830e7259c32baf0249f6ebff7e7614e6b
Menci <huanghaorui301@gmail.com>
2018-12-02 23:30:49 +0800

    Add NOI Linux C / C++ compilers

--- src/languages/cpp11-noilinux.ts
+++ src/languages/cpp11-noilinux.ts
@@ -1,7 +1,7 @@
-export const lang =  {
-    name: "java",
-    sourceFileName: "Main.java",
-    fileExtension: "java",
+export const lang = {
+    name: "cpp11-noilinux",
+    sourceFileName: "a.cpp",
+    fileExtension: "cpp",
     binarySizeLimit: 5000 * 1024,
 
     // Note that these two paths are in the sandboxed environment.
@@ -9,16 +9,17 @@ export const lang =  {
         // To customize the compilation process,
         // write a shell script or some other stuff, 
         // and put it to your sandbox.
-        executable: "/usr/bin/compile-java",
-        parameters: ["compile-java", sourcePath, outputDirectory],
+	executable: "/usr/bin/compile-cpp-noilinux",
+	parameters: ["compile-cpp-noilinux", "-std=c++11", sourcePath, "-o", `${outputDirectory}/a.out`, "-O2", "-DONLINE_JUDGE"],
         time: 5000,
         memory: 1024 * 1024 * 1024 * 2,
-        process: 30,
+        process: 10,
         // This is just a redirection. You can simply ignore this
         // if you can specify custom location for message output
         // in the parameter of the compiler, or have redirected the compilation 
         // message to somewhere.
         // An example will be available soon.
+        stdout: `${outputDirectory}/message.txt`,
         stderr: `${outputDirectory}/message.txt`,
         // We will read this file for message in the output directory.
         messageFile: 'message.txt',
@@ -33,11 +34,12 @@ export const lang =  {
         stdoutFile = null,
         stderrFile = null
     ) => ({
-        executable: `${binaryDirectory}/run`,
-        parameters: ["run"],
+        executable: `${binaryDirectory}/a.out`,
+        parameters: [],
         time: time,
         memory: memory,
-        process: 25,
+        stackSize: memory,
+        process: 1,
         stdin: stdinFile,
         stdout: stdoutFile,
         stderr: stderrFile,
--- src/languages/index.ts
+++ src/languages/index.ts
@@ -29,12 +29,15 @@ export interface Language {
 }
 
 export const languages: Language[] = [
-    require('./c'),
     require('./cpp'),
     require('./cpp11'),
     require('./cpp17'),
+    require('./cpp-noilinux'),
+    require('./cpp11-noilinux'),
     require('./cpp11-clang'),
     require('./cpp17-clang'),
+    require('./c'),
+    require('./c-noilinux'),
     require('./csharp'),
     require('./haskell'),
     require('./java'),
