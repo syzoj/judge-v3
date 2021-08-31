222ba6a7b3a8e094711b87e6483b80eb531a3f55
Menci <huanghaorui301@gmail.com>
2021-08-31 10:53:26 +0800

    Add environment variables config support

--- src/daemon/config.ts
+++ src/daemon/config.ts
@@ -2,6 +2,10 @@ import commandLineArgs = require('command-line-args');
 import fs = require('fs');
 import winston = require('winston');
 import { configureWinston } from '../winston-common';
+import { assign } from 'lodash';
+import objectPath = require('object-path');
+
+const daemonConfigExample = require("../../daemon-config-example.json");
 
 export interface ConfigStructure {
     serverUrl: string;
@@ -22,10 +26,11 @@ const optionDefinitions = [
 const options = commandLineArgs(optionDefinitions);
 
 function readJSON(path: string): any {
+    if (!fs.existsSync(path)) return {};
     return JSON.parse(fs.readFileSync(path, 'utf8'));
 }
 
-const configJSON = readJSON(options["config"]);
+const configJSON = assign({}, daemonConfigExample, readJSON(options["config"]));
 export const globalConfig: ConfigStructure = {
     serverUrl: configJSON.ServerUrl,
     serverToken: configJSON.ServerToken,
@@ -35,6 +40,20 @@ export const globalConfig: ConfigStructure = {
     redis: configJSON.RedisUrl,
     dataDisplayLimit: configJSON.DataDisplayLimit,
     tempDirectory: configJSON.TempDirectory
+}
+
+const configEnvOverrideItems = {
+    SYZOJ_JUDGE_WEB_URL: [String, "serverUrl"],
+    SYZOJ_WEB_SECRET_JUDGE: [String, "serverToken"],
+    SYZOJ_JUDGE_RABBITMQ_URI: [String, "rabbitMQ"],
+    SYZOJ_JUDGE_TESTDATA_PATH: [String, "testDataDirectory"],
+    SYZOJ_JUDGE_REDIS_URI: [String, "redis"]
+};
+
+for (const key in configEnvOverrideItems) {
+    const [Type, configKey] = configEnvOverrideItems[key];
+    if (key in process.env)
+        objectPath.set(globalConfig, configKey, Type(process.env[key]));
 }
 
 configureWinston(options.verbose);
